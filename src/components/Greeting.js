d3739228bd7d8f4ee31fa83ad82f0870f7fc32bf
Sergio <norgestream@gmail.com>
2024-06-10 16:37:22 +0300

    add test for user event

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [text, setText] = useState(false);
+
+  const handleChangeText = () => {
+    setText(true);
+  }
+
   return (
     <div>
       <h2>Hello World</h2>
-      <p>I am React developer. I can help you!</p>
+      <p>{text ? 'I love my job' : 'Nothing is impossible'}</p>
+      <button onClick={handleChangeText}>Change text</button>
     </div>
   );
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,29 @@
 import Greeting from './Greeting';
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-test('renders Hello world as a test', () => {
-  render(<Greeting />);
+describe('Greeting component', () => {
+  test('renders Hello world as a test', () => {
+    render(<Greeting />);
+  
+    const helloWorldElement = screen.getByText('Hello world', { exact: false});
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+  test('renders "Nothing is impossible" if the button was NOT clicked', () => {
+    render(<Greeting />)
 
-  const helloWorldElement = screen.getByText('Hello world', { exact: false});
-  expect(helloWorldElement).toBeInTheDocument();
-})
+    const outputElement = screen.getByText('Nothing is impossible', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+
+  });
+  test('renders "I love my job" if the button was clicked', () => {
+    render(<Greeting />)
+
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText('I love my job');
+    expect(outputElement).toBeInTheDocument();
+
+  });
+});
